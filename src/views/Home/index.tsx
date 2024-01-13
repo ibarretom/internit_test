27f589d0a5fa6e145fa0991aaf1a5e02b30b000f
ibarretom <igorbm.23@gmail.com>
2024-01-13 15:59:50 -0300

    animação banner home

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -3,8 +3,49 @@ import Lazer from "../../assets/images/home/lazer.png";
 import Memoria from "../../assets/images/home/memoria.png";
 import Decor from "../../assets/images/decor.svg";
 import { CTA } from "../../components/Buttons/CTA";
+import { useState } from "react";
+
+type TBanners = "Conforto" | "Lazer" | "Memoria";
+
+type TBannerElement = {
+  id: TBanners;
+  img: string;
+};
+class AvailableBanners {
+  private banners: TBannerElement[] = [
+    { id: "Conforto", img: Conforto },
+    { id: "Lazer", img: Lazer },
+    { id: "Memoria", img: Memoria },
+  ];
+  public readonly available: TBannerElement[] = structuredClone(this.banners);
+  private currentIndex: number = 0;
+
+  constructor(currentBanner: TBanners) {
+    this.currentIndex = this.banners.findIndex(
+      (banner) => banner.id == currentBanner
+    );
+  }
+
+  public next(): TBannerElement {
+    this.currentIndex = (this.currentIndex + 1) % this.banners.length;
+
+    return this.current();
+  }
+
+  public current(): TBannerElement {
+    return this.banners[this.currentIndex];
+  }
+}
 
 export function Home() {
+  const [currentBanner, setCurrentBanner] = useState<TBanners>("Conforto");
+
+  const banners = new AvailableBanners(currentBanner);
+
+  setInterval(() => {
+    setCurrentBanner(banners.next().id);
+  }, 2000);
+
   return (
     <section id="home">
       <main className="home__hero">
@@ -28,9 +69,17 @@ export function Home() {
       </main>
 
       <div className="home__banner">
-        <img src={Conforto} alt="Sala de estar" />
-        <img src={Lazer} alt="Sala de estar" />
-        <img src={Memoria} alt="Sala de estar" />
+        {banners.available.map((banner: TBannerElement) => {
+          return (
+            <img
+              className="home__banner-image"
+              src={banner.img}
+              alt="Sala de estar"
+              data-visible={currentBanner === banner.id}
+              key={banner.id}
+            />
+          );
+        })}
       </div>
 
       <img src={Decor} className="home__hero-flying-image" />
